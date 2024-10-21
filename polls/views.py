d2cdb442d2a3873d0a2e5e0a7adb8a13e2cc78b8
Pooja Thakur <poojauthakur00@gmail.com>
2024-10-20 23:30:42 -0400

    Updated Python files to fix issues

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404
-from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+from django.http import HttpResponseRedirect
 
 
 from .models import Choice, Question
